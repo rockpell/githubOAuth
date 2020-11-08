a770bc7bb5ea7ab3dc7564b31c09417211b522c5
chlim <chlim@student.42seoul.kr>
2020-11-08 13:45:25 +0900

    [Add] npm package
    add eslint
    add prettier
    add back
    add front

---
+++ back/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const passport = require('passport');
+
+require('dotenv').config();
+
+const indexRouter = require('./routes/index');
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.use(passport.initialize());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/', indexRouter);
+
+app.listen(port, () => {
+  console.log(`server is running on ${port}`);
+});
